b3552f642ce5ce8d4adb7e5ee1f4e09a0bbb5975
Gerardo Jaramillo <gerardo.jaramillo@gmail.com>
2018-12-03 09:57:52 -0600

    Database Connection

---
+++ app.py
@@ -0,0 +1,10 @@
+import pymysql.cursors
+
+connection = pymysql.connect(host='localhost',
+                             user='user',
+                             password='passwd',
+                             db='db',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+print(connection)
