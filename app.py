b54a4952f212918caeff09522ac9aae00ab1c9ca
Gerardo Jaramillo <gerardo.jaramillo@gmail.com>
2018-12-04 18:05:58 -0600

    new product configuration

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ import requests
 class GetAppConf:
     # Lets create object to interact with the database
     def app_configuration(self):
-        app_settings = []
-        categories_display = []
         try:
             connection = Database().connect_to_database()
             cursor = connection.cursor()
@@ -32,32 +30,34 @@ class GetAppConf:
                 "select id, name, description, icon, created_at from vertical")
             verticals = cursor.fetchall()
 
+            app_settings = []
+            categories_display = []
             verts = {}
             for vertical in verticals:
                 app_setting = {}
                 app_setting['name'] = vertical['name']
                 app_setting['icon'] = vertical['icon']
-                products_vertical = {}
+                prods = []
                 for product in products:
-                    products_vertical['name'] = product['name']
-                    products_vertical['icon'] = product['icon']
-                    products_vertical['description'] = product['description']
-                    categories_array = []
-                    lifecycle_array = []
-                    filters_array = []
-                    for category in categories:
-                        category_display = {}
-                        category_display['description'] = category['description']
-                        category_display['name'] = category['name']
-                        categories_array.append(category_display)
-                    products_vertical['categories'] = categories_array
-                    for lifecycle in lifecycles:
-                        lifecycle_array.append(lifecycle['name'])
-                    for filter in filters:
-                        filters_array.append(filter['name'])
-                app_setting['products'] = products_vertical
-                app_setting['lifecycle'] = lifecycle_array
-                app_setting['filters'] = filters_array
+                    products_vertical = {}
+                    if vertical['id'] == product['vertical_id']:
+                        products_vertical['name'] = product['name']
+                        products_vertical['icon'] = product['icon']
+                        products_vertical['description'] = product['description']
+                        categories_array = []
+                        lifecycle_array = []
+                        filters_array = []
+                        for category in categories:
+                            categories_array.append(category['name'])
+                        products_vertical['categories'] = categories_array
+                        for lifecycle in lifecycles:
+                            lifecycle_array.append(lifecycle['name'])
+                        products_vertical['lifecycles'] = lifecycle_array
+                        for filter in filters:
+                            filters_array.append(filter['name'])
+                        products_vertical['filters'] = filters_array
+                        prods.append(products_vertical)
+                app_setting['products'] = prods
                 app_settings.append(app_setting)
                 verts['verticals'] = app_settings
             json_object = json.dumps(verts)
